3e1159ff2a5e7839742f50a77b75a15c3f64d124
Gheeno San Pascual <gino.sanpascual@gmail.com>
2023-12-03 20:02:14 -0700

    LINK-02 LinkedIn learning.

---
+++ linkedin_learning/python/template_strings.py
@@ -0,0 +1,11 @@
+from string import Template
+
+def main():
+    str1 = "Hello {0}, this is an example of {1}".format("World!", "Template Strings")
+    print(str1)
+
+    templ = Template("Hello ${world}")
+
+
+if __name__ == "__main__":
+    main()
