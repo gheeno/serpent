b0512af6fbedd1a7d35c3111a0ccc86714454517
Gheeno San Pascual <gino.sanpascual@gmail.com>
2024-02-28 20:54:14 -0700

    UDEM-09 adds nested_dictionary sample

---
+++ udemy_projects/day9/nested_dictionary.py
@@ -0,0 +1,23 @@
+travel_log = {
+    "Philippines" : {
+        "cities_visited" : ["Manila", "El Nido", "Siargao"],
+        "total_visits" : 20
+    },
+    "Canada": {
+        "cities_visited" : ["Calgary", "Toronto", "Vancouver"],
+        "total_visits" : 100
+    }
+}
+
+travel_log_list = [
+    { 
+        "Country" : "Philippines",
+        "cities_visited" : ["Manila", "El Nido", "Siargao"],
+        "total_visits" : 20
+    },
+    {
+        "Country" : "Canada",
+        "cities_visited" : ["Calgary", "Toronto", "Vancouver"],
+        "total_visits" : 100
+    },
+]
